8ec6be55b1f5c91398e2155312641b21e667313f
Umesh Singh <singhuh@mail.uc.edu>
2019-10-02 08:13:11 -0400

    Updated 100219

---
+++ CoreCountry/Migrations/20191002075342_New.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreCountry.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Destination",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityCode",
+                table: "Destination",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 13);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TravelDate",
+                table: "Destination",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TravelDate",
+                table: "Destination");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Destination",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CityCode",
+                table: "Destination",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 3);
+        }
+    }
+}
--- CoreCountry/Migrations/20191002081646_Third.Designer.cs
+++ CoreCountry/Migrations/20191002081646_Third.Designer.cs
@@ -4,14 +4,16 @@ using CoreCountry.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreCountry.Migrations
 {
     [DbContext(typeof(CoreCountryContext))]
-    partial class CoreCountryContextModelSnapshot : ModelSnapshot
+    [Migration("20191002081646_Third")]
+    partial class Third
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
